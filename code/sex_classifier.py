d4e7ee9a84b4956b6f7a43cbd6af7cacba740413
Andreas Madsen <amwebdk@gmail.com>
2016-03-11 11:39:33 +0100

    [refactor] cleanup and add loss plots

--- code/sex_classifier.py
+++ code/sex_classifier.py
@@ -1,13 +1,11 @@
 
-import timit
-
 import numpy as np
 import lasagne
 import theano
 import theano.tensor as T
-import scipy.signal
-import scipy.io.wavfile
+import matplotlib.pyplot as plt
 
+import timit
 from helpers.minibatch_spectogram import MinibatchSpectogram
 
 # create Theano variables for input and target minibatch
@@ -15,68 +13,80 @@ input_var = T.tensor4('X')
 target_var = T.ivector('y')
 
 # create a small convolutional neural network
-network = lasagne.layers.InputLayer(shape=(None, 1, 129, 250), input_var=input_var)
+network = lasagne.layers.InputLayer(shape=(None, 1, 257, 150), input_var=input_var)
 
-network = lasagne.layers.Conv2DLayer(network, num_filters=16, filter_size=(16, 16),
-                                     nonlinearity=lasagne.nonlinearities.leaky_rectify)
+network = lasagne.layers.Conv2DLayer(network, num_filters=32, filter_size=(257, 8),
+                                     nonlinearity=lasagne.nonlinearities.sigmoid)
 
-network = lasagne.layers.Conv2DLayer(network, num_filters=2, filter_size=(3, 3),
-                                     nonlinearity=lasagne.nonlinearities.leaky_rectify)
-
-network = lasagne.layers.Pool2DLayer(network, (3, 3), stride=2, mode='max')
+network = lasagne.layers.MaxPool2DLayer(network, (1, 4))
 
 network = lasagne.layers.DenseLayer(network, num_units=32,
-                                    nonlinearity=lasagne.nonlinearities.leaky_rectify,
-                                    W=lasagne.init.Orthogonal())
+                                    nonlinearity=lasagne.nonlinearities.sigmoid)
 
 network = lasagne.layers.DenseLayer(network, num_units=2,
-                                    nonlinearity=lasagne.nonlinearities.softmax,
-                                    W=lasagne.init.Orthogonal())
+                                    nonlinearity=lasagne.nonlinearities.softmax)
 
 # create loss function
-prediction = lasagne.layers.get_output(network)
-loss = lasagne.objectives.categorical_crossentropy(prediction, target_var)
-loss = loss.mean()
-
-# create parameter update expressions
-params = lasagne.layers.get_all_params(network, trainable=True)
-updates = lasagne.updates.nesterov_momentum(loss, params,
-                                            learning_rate=0.01, momentum=0.9)
-
-# compile training function that updates parameters and returns training loss
-train_fn = theano.function([input_var, target_var], loss, updates=updates)
-
-# train network (assuming you've got some training data in numpy arrays)
-print("training network")
-train_selector = MinibatchSpectogram(
-    timit.FileSelector(usage='train', dialect='dr1', texttype='SI'))
-for epoch in range(10):
-    loss = 0
-    items = 0
-
-    for (batch_input, batch_target) in train_selector:
-        items += batch_input.shape[0]
-        loss += train_fn(batch_input, batch_target)
-        print(loss)
-
-    print("Epoch %d: Loss %g" % (epoch + 1, loss / items))
+train_prediction = lasagne.layers.get_output(network)
+train_loss = lasagne.objectives.categorical_crossentropy(train_prediction, target_var)
+train_loss = train_loss.mean()
 
 # use trained network for predictions
 test_prediction = lasagne.layers.get_output(network, deterministic=True)
-predict_fn = theano.function([input_var], T.argmax(test_prediction, axis=1))
+test_loss = lasagne.objectives.categorical_crossentropy(test_prediction, target_var)
+test_loss = test_loss.mean()
 
-# Print predictions
-print("predicting network")
-test_selector = timit.FileSelector(usage='test', dialect='dr1', texttype='SI')
-for item in test_selector:
+# create parameter update expressions
+params = lasagne.layers.get_all_params(network, trainable=True)
+updates = lasagne.updates.nesterov_momentum(train_loss, params,
+                                            learning_rate=0.001, momentum=0.9)
 
-    # Get spectogram
-    spectogram = spectogram()
-    if (spectogram.shape[1] < 250): continue
-    spectogram = spectogram[:, 0:250]
-    spectogram = spectogram.reshape(1, 1, *spectogram.shape)
+# compile functions
+train_fn = theano.function([input_var, target_var], train_loss, updates=updates)
+predict_fn = theano.function([input_var], test_prediction)
+loss_fn = theano.function([input_var, target_var], test_loss)
 
-    print("(%s, %s): %d (actual: %s)" % (
-        item.speaker, item.sentense,
-        ['m', 'f'][predict_fn(spectogram)], item.sex
-    ))
+# File selectors
+test_selector = MinibatchSpectogram(
+    timit.FileSelector(usage='test', dialect='dr1'))
+
+train_selector = MinibatchSpectogram(
+    timit.FileSelector(usage='train', dialect='dr1'))
+
+# train network (assuming you've got some training data in numpy arrays)
+print("training network")
+
+train_loss_arr = np.zeros(100)
+test_loss_arr = np.zeros(100)
+epoch_arr = np.arange(1, 100 + 1)
+
+fig, ax = plt.subplots()
+train_points, = ax.plot(epoch_arr, train_loss_arr, label='train')
+test_points, = ax.plot(epoch_arr, test_loss_arr, label='test')
+plt.ylim(0, 1)
+plt.legend()
+plt.ion()
+
+for epoch in range(100):
+    train_loss = 0
+    test_loss = 0
+    items = 0
+
+    for (train_data, test_data) in zip(train_selector, test_selector):
+        train_batch_loss = train_fn(*train_data)
+        test_batch_loss = loss_fn(*test_data)
+        print(train_batch_loss, test_batch_loss)
+
+        items += 1
+        train_loss += train_batch_loss
+        test_loss += test_batch_loss
+
+    print("Epoch %d: Train Loss %g, Test Loss %g" % (
+          epoch + 1, train_loss / items, test_loss / items))
+
+    train_loss_arr[epoch] = train_loss / items
+    test_loss_arr[epoch] = test_loss / items
+
+    train_points.set_data(epoch_arr, train_loss_arr)
+    test_points.set_data(epoch_arr, test_loss_arr)
+    plt.pause(0.1)
