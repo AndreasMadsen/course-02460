1ae54799b58ae15ffaa0b59fe6c78349191ffe15
Andreas Madsen <amwebdk@gmail.com>
2016-03-13 16:44:59 +0100

    [refactor] update sex classifier to use new network class

--- code/sex_classifier.py
+++ code/sex_classifier.py
@@ -6,52 +6,18 @@ import theano.tensor as T
 import matplotlib.pyplot as plt
 
 import timit
-from helpers.minibatch_spectogram import MinibatchSpectogram
+import network
+import helpers
 
-# create Theano variables for input and target minibatch
-input_var = T.tensor4('X')
-target_var = T.ivector('y')
-
-# create a small convolutional neural network
-network = lasagne.layers.InputLayer(shape=(None, 1, 129, 300), input_var=input_var)
-
-network = lasagne.layers.Conv2DLayer(network, num_filters=16, filter_size=(129, 8),
-                                     nonlinearity=lasagne.nonlinearities.sigmoid)
-
-network = lasagne.layers.MaxPool2DLayer(network, (1, 4))
-
-network = lasagne.layers.DenseLayer(network, num_units=32,
-                                    nonlinearity=lasagne.nonlinearities.sigmoid)
-
-network = lasagne.layers.DenseLayer(network, num_units=2,
-                                    nonlinearity=lasagne.nonlinearities.softmax)
-
-# create loss function
-train_prediction = lasagne.layers.get_output(network)
-train_loss = lasagne.objectives.categorical_crossentropy(train_prediction, target_var)
-train_loss = train_loss.mean()
-
-# use trained network for predictions
-test_prediction = lasagne.layers.get_output(network, deterministic=True)
-test_loss = lasagne.objectives.categorical_crossentropy(test_prediction, target_var)
-test_loss = test_loss.mean()
-
-# create parameter update expressions
-params = lasagne.layers.get_all_params(network, trainable=True)
-updates = lasagne.updates.nesterov_momentum(train_loss, params,
-                                            learning_rate=0.001, momentum=0.9)
-
-# compile functions
-train_fn = theano.function([input_var, target_var], train_loss, updates=updates)
-predict_fn = theano.function([input_var], test_prediction)
-loss_fn = theano.function([input_var, target_var], test_loss)
+cnn = network.SimpleCNN(input_shape=(1, 129, 300), output_units=2, verbose=True)
+cnn.compile()
 
 # File selectors
-test_selector = MinibatchSpectogram(
+test_selector = helpers.MinibatchSpectogram(
     timit.FileSelector(usage='test', dialect='dr1'),
     nfft=256, truncate_time=300, noverlap=128)
 
-train_selector = MinibatchSpectogram(
+train_selector = helpers.MinibatchSpectogram(
     timit.FileSelector(usage='train', dialect='dr1'),
     nfft=256, truncate_time=300, noverlap=128)
 
@@ -77,11 +43,11 @@ for epoch in range(100):
     test_batches = 0
 
     for train_data in train_selector:
-        train_loss += train_fn(*train_data)
+        train_loss += cnn.train(*train_data)
         train_batches += 1
 
     for test_data in test_selector:
-        test_loss += loss_fn(*train_data)
+        test_loss += cnn.loss(*train_data)
         test_batches += 1
 
     print("Epoch %d: Train Loss %g, Test Loss %g" % (
