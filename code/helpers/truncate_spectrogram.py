72b5b2eee73058375333cc186bcc7ecc9ffe6465
Andreas Madsen <amwebdk@gmail.com>
2016-03-17 15:45:03 +0100

    [feature] add dileman log transform

--- code/helpers/truncate_spectrogram.py
+++ code/helpers/truncate_spectrogram.py
@@ -1,8 +1,11 @@
 
+import numpy as np
+
 class TruncateSpectrogram:
-    def __init__(self, selector, truncate=150, **kwargs):
+    def __init__(self, selector, log_transform=False, truncate=150, **kwargs):
         self._selector = selector
         self._truncate = truncate
+        self._log_transform = log_transform
         self._spectrogram_settings = kwargs
 
     def __iter__(self):
@@ -12,5 +15,8 @@ class TruncateSpectrogram:
             if (spectrogram.shape[1] >= self._truncate):
                 spectrogram = spectrogram[:, 0:self._truncate]
                 spectrogram = spectrogram.reshape(1, *spectrogram.shape)
+
+                if (self._log_transform):
+                    spectrogram = np.log(1 + 10000 * spectrogram)
 
                 yield (spectrogram, int(item.sex == 'f'))
