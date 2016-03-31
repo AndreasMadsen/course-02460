5083f4bc2ab11f69628e0df17f444128c22ed17d
Andreas Madsen <amwebdk@gmail.com>
2016-03-31 15:22:51 +0200

    [fix] use numpy mean and std

--- code/helpers/normalize.py
+++ code/helpers/normalize.py
@@ -4,11 +4,27 @@ import math
 import numpy as np
 
 class Normalize:
-    def __init__(self, iterable):
+    def __init__(self, iterable, mean=None, stddev=None):
         self._iterable = iterable
+
+        if mean is None or stddev is None:
+            inputs = np.asarray([input for input, target in iterable])
+
+        if mean is None:
+            self.mean = np.mean(inputs.ravel())
+        else:
+            self.mean = mean
+
+        if stddev is None:
+            self.stddev = np.std(inputs.ravel())
+        else:
+            self.stddev = stddev
 
         # Estimate mean and variance using the online Knuth-Welford algorithm
         # https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Online_algorithm
+        # TODO: estimate mean and stddev using Knuth-Welford to prevent numerical
+        # errors and avoid buffering of all the data.
+        """
         n = 0
         mean = 0.0
         M2 = 0.0
@@ -24,6 +40,7 @@ class Normalize:
         self.mean = mean
         self.variance = M2 / (n - 1)
         self.stddev = math.sqrt(self.variance)
+        """
 
     def __iter__(self):
         for input, target in self._iterable:
