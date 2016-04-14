f90ab46aa64cbb96f947c42de7974ae4bc89d6ca
Lasse Regin Nielsen <lasseregin@gmail.com>
2016-04-15 00:06:19 +0200

    [Add] Removed speakers not represented more than 10 times.

--- code/speaker_classifier_overview.py
+++ code/speaker_classifier_overview.py
@@ -6,17 +6,17 @@ import helpers
 
 # Create data selector object
 selector = timit.FileSelector(dialect=None)
+#selector = timit.FileSelector(dialect='dr1')
 selector = helpers.TargetType(selector, target_type='speaker')
 speakers = selector.labels
 selector = helpers.Spectrogram(selector, nperseg=256, noverlap=128, normalize_signal=True)
 selector = helpers.Truncate(selector, truncate=300, axis=2)
 selector = helpers.Normalize(selector)
+selector = helpers.Filter(selector, min_count=10)
 selector = helpers.Validation(selector, test_fraction=0.25, stratified=True)
 
 train_selector = helpers.Minibatch(selector.train)
 test_selector  = helpers.Minibatch(selector.test)
-
-# TODO: Count in train and test
 
 # Count speakers
 speakers_count = np.zeros(shape=len(speakers.keys()))
@@ -35,14 +35,27 @@ for _, target in test_selector:
 
 # Print speakers count result
 for speaker, count in zip(speakers, speakers_count_test):
-    print('Speaker train: %s, count: %d' % (speaker, count))
+    if (count == 0):
+        print('Speaker train: %s was removed in the filter function' % (speaker))
+    else:
+        print('Speaker train: %s, count: %d' % (speaker, count))
 print('')
 for speaker, count in zip(speakers, speakers_count_train):
-    print('Speaker test: %s, count: %d' % (speaker, count))
+    if (count == 0):
+        print('Speaker test: %s was removed in the filter function' % (speaker))
+    else:
+        print('Speaker test: %s, count: %d' % (speaker, count))
 print('')
+
+# Remove speakers with 0 counts.
+speakers_count = np.delete(speakers_count, np.where(speakers_count == 0))
+speakers_count_train = np.delete(speakers_count_train, np.where(speakers_count_train == 0))
+speakers_count_test = np.delete(speakers_count_test, np.where(speakers_count_test == 0))
 
 # Other details
 print('Number of speakers: %d' % (len(speakers)))
+print('Number of active speakers: %d' % (len(speakers_count)))
+print('Number of observations filtered: %d' % (np.sum(speakers_count)))
 print('min speaker count: %d' % (np.min(speakers_count)))
 print('min speaker count train: %d' % (np.min(speakers_count_train)))
 print('min speaker count test: %d' % (np.min(speakers_count_test)))
