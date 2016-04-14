928a3550673760eb2979248c0aa3e35e2315582e
Lasse Regin Nielsen <lasseregin@gmail.com>
2016-04-14 14:40:37 +0200

    [Add] Stratified sampling.

--- code/helpers/validation.py
+++ code/helpers/validation.py
@@ -2,21 +2,79 @@
 import numpy as np
 
 class Validation:
-    def __init__(self, selector, test_fraction=0.33, **kwargs):
+    def __init__(self, selector, test_fraction=0.33, stratified=False, **kwargs):
         self.selector = selector
-        n = sum([1 for _ in selector])
+
+        # Load all targets (labels)
+        labels = [target for (_, target) in self.selector]
+
+        # Create shuffled index array
+        n = len(labels)
         self.indices = np.arange(n)
         np.random.shuffle(self.indices)
         self.n_split = int(n * test_fraction)
 
+        print('n = %d' % (n))
+        print(np.max(self.indices))
+
+        # Create idx splitting mapping
+        # 0 = Train
+        # 1 = Test
+        split_idx = {}
+
+        if stratified:
+            # Make label counter for train and test
+            labels_unique = sorted(list(set(labels)))
+            label_count_train = { label: 0 for label in labels_unique }
+            label_count_test  = { label: 0 for label in labels_unique }
+
+            #for i, idx in enumerate(self.indices):
+            for i in range(0, n):
+                idx = self.indices[i]
+                label = labels[idx]
+
+                # Make sure atleast 1 of each label is present in train and test
+                if label_count_train[label] == 0:
+                    split_idx[idx] = 0
+                    label_count_train[label] += 1
+                    continue
+
+                if label_count_test[label] == 0:
+                    split_idx[idx] = 1
+                    label_count_test[label] += 1
+                    continue
+
+                # If one label already is present in train and test, add label
+                # according to the fraction of labels in test.
+                label_test_fraction = float(label_count_test[label]) / float(label_count_train[label] + label_count_test[label])
+
+                if label_test_fraction > test_fraction:
+                    split_idx[idx] = 0
+                    label_count_train[label] += 1
+                else:
+                    split_idx[idx] = 1
+                    label_count_test[label] += 1
+
+            print(label_count_train)
+            print(label_count_test)
+
+        else:
+            # Simple split
+            for idx in self.indices:
+                split_idx[idx] = int(idx < self.n_split)
+
+        print(split_idx.keys())
+
+        self.split_idx = split_idx
+
     @property
     def train(self):
         for i, (input, target) in enumerate(self.selector):
-            if (self.indices[i] >= self.n_split):
+            if (self.split_idx[self.indices[i]] == 0):
                 yield input, target
 
     @property
     def test(self):
         for i, (input, target) in enumerate(self.selector):
-            if (self.indices[i] < self.n_split):
+            if (self.split_idx[self.indices[i]] == 1):
                 yield input, target
--- code/speaker_classifier_overview.py
+++ code/speaker_classifier_overview.py
@@ -11,24 +11,46 @@ speakers = selector.labels
 selector = helpers.Spectrogram(selector, nperseg=256, noverlap=128, normalize_signal=True)
 selector = helpers.Truncate(selector, truncate=300, axis=2)
 selector = helpers.Normalize(selector)
-selector = helpers.Validation(selector, test_fraction=0.25)
+selector = helpers.Validation(selector, test_fraction=0.25, stratified=True)
+
+#import sys
+#sys.exit()
+
+
 train_selector = helpers.Minibatch(selector.train)
 test_selector  = helpers.Minibatch(selector.test)
 
+# TODO: Count in train and test
+
 # Count speakers
 speakers_count = np.zeros(shape=len(speakers.keys()))
-for selector in [train_selector, test_selector]:
-    for _, target in selector:
-        for item in target:
-            speakers_count[item] += 1
+speakers_count_train = np.zeros(shape=len(speakers.keys()))
+speakers_count_test  = np.zeros(shape=len(speakers.keys()))
+
+for _, target in train_selector:
+    for item in target:
+        speakers_count_train[item] += 1
+        speakers_count[item] += 1
+
+for _, target in test_selector:
+    for item in target:
+        speakers_count_test[item] += 1
+        speakers_count[item] += 1
 
 # Print speakers count result
-for speaker, count in zip(speakers, speakers_count):
-    print('Speaker: %s, count: %d' % (speaker, count))
+for speaker, count in zip(speakers, speakers_count_test):
+    print('Speaker train: %s, count: %d' % (speaker, count))
+print('')
+for speaker, count in zip(speakers, speakers_count_train):
+    print('Speaker test: %s, count: %d' % (speaker, count))
 print('')
 
 # Other details
 print('Number of speakers: %d' % (len(speakers)))
 print('min speaker count: %d' % (np.min(speakers_count)))
+print('min speaker count train: %d' % (np.min(speakers_count_train)))
+print('min speaker count test: %d' % (np.min(speakers_count_test)))
 print('max speaker count: %d' % (np.max(speakers_count)))
+print('max speaker count train: %d' % (np.max(speakers_count_train)))
+print('max speaker count test: %d' % (np.max(speakers_count_test)))
 print('mean speaker counts: %d' % (np.mean(speakers_count)))
