0b7840420d35e6075c9028de6abb2485cd883608
Andreas Madsen <amwebdk@gmail.com>
2016-04-20 13:36:10 +0200

    [refactor] simplify and speedup filter

--- code/helpers/filter.py
+++ code/helpers/filter.py
@@ -1,29 +1,17 @@
 
+import collections
+
 class Filter:
     def __init__(self, selector, min_count=1):
         self._selector = selector
+        self._min_count = min_count
 
-        count = 0
-        class_idx = {}
+        # Count number of observations in each label
+        self._class_count = collections.Counter()
         for i, (_, target) in enumerate(selector):
-            if (target not in class_idx.keys()):
-                class_idx[target] = []
-
-            # Append index
-            class_idx[target].append(i)
-            count += 1
-
-        # Allowed indices
-        indices_allowed = list(range(0, count))
-
-        # Remove classes not satisfying filter criterias
-        for target, indices in class_idx.items():
-            if (len(indices) < min_count):
-                for idx in indices:
-                    indices_allowed.pop(indices_allowed.index(idx))
-        self.indices_allowed = indices_allowed
+            self._class_count[target] += 1
 
     def __iter__(self):
-        for i, item in enumerate(self._selector):
-            if (i in self.indices_allowed):
-                yield item
+        for input, target in self._selector:
+            if (self._class_count[target] >= self._min_count):
+                yield (input, target)
