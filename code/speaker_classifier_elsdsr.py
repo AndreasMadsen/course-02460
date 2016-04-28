09ee2fcb8afcc82831d6c669f4711435fbc45102
Lasse Regin Nielsen <lasseregin@gmail.com>
2016-04-28 19:38:13 +0200

    Removed ProNN from the file.

--- code/speaker_classifier_elsdsr.py
+++ code/speaker_classifier_elsdsr.py
@@ -26,10 +26,8 @@ selector = helpers.Validation(selector, test_fraction=0.25, stratified=True)
 train_selector = helpers.Minibatch(selector.train)
 test_selector  = helpers.Minibatch(selector.test)
 
-#cnn = network.Logistic(input_shape=(1, 129, 300), output_units=len(speakers),
-#                       verbose=True, learning_rate=0.1)
-cnn = network.ProNN(input_shape=(1, 129, 300), output_units=len(speakers),
-                       verbose=True, learning_rate=0.01)
+cnn = network.Logistic(input_shape=(1, 129, 300), output_units=len(speakers),
+                       verbose=True, learning_rate=0.1)
 #cnn = network.DielemanCNN(input_shape=(1, 129, 300), output_units=len(speakers),
 #                          verbose=True, learning_rate=0.001, dropout=True)
 cnn.add_regularizer(network.regularizer.ScaleInvariant(1e-1))
