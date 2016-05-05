961bd7d41e63c83807ce8bc878fb0df5994daf21
Lasse Regin Nielsen <lasseregin@gmail.com>
2016-05-05 15:45:52 +0200

    [Feature] Added offset invariant class.

--- code/network/regularizer/__init__.py
+++ code/network/regularizer/__init__.py
@@ -1,3 +1,4 @@
 
 from network.regularizer.weight_decay import WeightDecay
 from network.regularizer.scale_invariant import ScaleInvariant
+from network.regularizer.offset_invariant import OffsetInvariant
