37056c927f7bf224b93317dae2ca3739ed4b8735
Lasse Regin Nielsen <lasseregin@gmail.com>
2016-05-11 11:14:15 +0200

    Changed the convolutional figure.

--- code/speaker_classifier_elsdsr.py
+++ code/speaker_classifier_elsdsr.py
@@ -16,7 +16,7 @@ import early_stopping
 selector = timit.FileSelector()
 selector = helpers.TargetType(selector, target='speaker')
 speakers = selector.labels
-selector = helpers.Spectrogram(selector, nperseg=256, noverlap=128, normalize_signal=True)
+selector = helpers.Spectrogram(selector, nperseg=256, noverlap=128, normalize_signal=True, log_transform=True)
 selector = helpers.Splitter(selector, split_size=100, axis=2)
 selector = helpers.Normalize(selector)
 selector = helpers.Validation(selector, test_fraction=0.25, stratified=True)
@@ -24,12 +24,13 @@ selector = helpers.Validation(selector, test_fraction=0.25, stratified=True)
 train_selector = helpers.Minibatch(selector.train)
 test_selector  = helpers.Minibatch(selector.test)
 
-cnn = network.Logistic(input_shape=(1, 129, 100), output_units=len(speakers), verbose=True)
-# cnn = network.DielemanCNN(input_shape=(1, 129, 100), output_units=len(speakers), verbose=True)
-# cnn.add_regularizer(network.regularizer.WeightDecay(1e-1))
-# cnn.add_regularizer(network.regularizer.ScaleInvariant(1e-1))
-# cnn.add_regularizer(network.regularizer.OffsetInvariant(1e-1))
-
+#cnn = network.Logistic(input_shape=(1, 129, 100), output_units=len(speakers),
+#                       verbose=True, learning_rate=0.1)
+cnn = network.DielemanCNN(input_shape=(1, 129, 100), output_units=len(speakers),
+                          verbose=True, learning_rate=0.001, dropout=True)
+cnn.add_regularizer(network.regularizer.WeightDecay(1e-2))
+#cnn.add_regularizer(network.regularizer.ScaleInvariant(1e-1))
+#cnn.add_regularizer(network.regularizer.OffsetInvariant(1e+1, use_Rop=True))
 cnn.compile()
 
 epochs = 500
